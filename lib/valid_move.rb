00f57055973e4867e05b55f52426d2e98fcfd6da
Jesse Wenger <wengerj@gmail.com>
2021-02-24 16:24:51 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move? (board, index)
-  if index <= 0 && index >= 8
+  if index.between (0, 8) <= 0 && index >= 8
     false
   elsif position_taken?
     false
